d2dfd36517ecdb2343cf7729a5fc34ea83ef79b4
Abhishek <abhisheknodejsdeveloper@gmail.com>
2025-12-30 07:02:25 +0000

    bubbleSort header file

---
+++ bubbleSort.h
@@ -0,0 +1,22 @@
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+
+void bubbleSort(int arr[], int n){
+    for(int i=0; i<n-1;i++){
+
+        bool swapped = false;
+        for(int j =0; j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                //Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
+            }
+        }
+
+        if(!swapped)break;
+    }
+}
+
+#endif
