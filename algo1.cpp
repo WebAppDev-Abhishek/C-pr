e0687dcc30fc538be135c46c8883166188f83784
Abhishek <abhisheknodejsdeveloper@gmail.com>
2025-12-30 05:26:10 +0000

    update main function

--- algo1.cpp
+++ algo1.cpp
@@ -13,14 +13,43 @@ class stack {
 public:
     stack(): topNode(nullptr){}
 
-    void createNode(int value){
+    //Push oparation
+    void push(int value){
         Node* newNode = new Node();
         newNode->data = value;
         newNode->next = topNode;
         topNode = newNode;
-        cout<<"New node are: "<<value<<"\npush as top node"<<endl;
+        cout<<"Element "<<value<<"\nPushed to stack"<<endl;
     }
 
+    //Pop operation
+    void pop(){
+        if(isEmpty()){
+            cout<<"Stack Underflow! Nothing to pop."<<endl;
+            return;
+        }
+
+        Node* temp = topNode;
+        int poppedVal = temp->data;
+        topNode=topNode->next;
+        delete temp;
+        cout<<"Element "<<poppedVal<<"popped from stack."<<endl;
+    }
+
+    //Peek/Top operation
+    void peek(){
+        if(isEmpty()){
+            cout<<"Stack is empty."<<endl;
+        }else{
+            cout<<"Top element is: "<<topNode->data<<endl;
+        }
+    }
+
+    bool isEmpty(){
+        return topNode == nullptr;
+    }
+
+    //Display all elements
     void display(){
         Node* temp = topNode;
         while(temp!=nullptr){
@@ -32,19 +61,37 @@ public:
 
 int main(){
     stack st;
-    int count, val;
+    int choice, val;
 
-    cout<<"Enter the number of nodes:"<<endl;
-    cin>>count;
+    cout<<"----Linked lsit Stack Menu------"<<endl;
+    do{
+        cout<<"\n1. Push\n2. Pop\n3. Peek(top)\n4. Display stack\n5. Exit\n";
+        cout<<"Enter your choice: ";
+        cin>>choice;
 
-    for(int i = 0; i<count; i++){
-        cout<<"Enter the node data"<<(i+1)<<" ";
-        cin>>val;
-        st.createNode(val);
-    }
-
-    cout<<"This is a stack:"<<endl;
-    st.display();
+        switch(choice){
+            case 1:
+                cout<<"Enter the value to push: ";
+                cin>>val;
+                st.push(val);
+                break;
+            case 2:
+                st.pop();
+                break;
+            case 3:
+                st.peek();
+                break;
+            case 4:
+                st.display();
+                break;
+            case 5:
+                cout<<"Exiting..."<<endl;
+                break;
+            default:
+                cout<<"Invalid choice! Please try again "<<endl;
+        }
+    }while(choice!=5);
 
     return 0;
+    
 }
