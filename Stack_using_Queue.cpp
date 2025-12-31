f19b3535ebbe25800e93782c936fda7b3e396996
Abhishek <abhisheknodejsdeveloper@gmail.com>
2025-12-31 08:28:34 +0000

    enqueue function created

--- Stack_using_Queue.cpp
+++ Stack_using_Queue.cpp
@@ -4,4 +4,26 @@
 class Queue {
     int arr[MAX];
     int front, rear;
+
+    public:
+    Queue() {
+        front = -1;
+        rear = -1;
+    }
+
+    bool isFull(){ return (front == 0 && rear == MAX - 1) || (rear == (front - 1) % (MAX - 1));}
+    bool isEmpty() {return front == -1; }
+
+    void enqueue(int value){
+        if(isFull()){
+            std::cout <<"Queue Overflow\n";
+            return;
+        }
+
+        if(front == -1) front = 0; //first element
+        rear = (rear + 1) % MAX;
+        arr[rear] = value;
+    }
+
+    
 }
